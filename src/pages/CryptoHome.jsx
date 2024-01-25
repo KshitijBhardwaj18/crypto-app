81346aa17f93e9394ba7f8b1c8236b974056e57d
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-25 10:33:00 +0530

    Made a trending component Lab6 completed

--- src/pages/CryptoHome.jsx
+++ src/pages/CryptoHome.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
+import Trending from '../components/trending'
 
 const CryptoHome = () => {
   return (
-    <div>
-      
+    <div className='wrapper-container'>
+      <Trending/>
     </div>
   )
 }
