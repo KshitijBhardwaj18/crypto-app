e9ec9dc61cddc31bd17f3891c4cd841d71fe1ae0
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-01-25 12:32:44 +0530

    Fixed lab7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import CryptoDetail from "./pages/CryptoDetail"
 import CryptoHome from "./pages/CryptoHome"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/navbar"
 
 function App() {
   
